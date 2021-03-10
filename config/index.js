113732a523916fde0b9695fa31a3ab89b444e65b
Hiroyasu Yamada <hiroyasu.yamada@logicheart.biz>
2021-03-10 21:36:08 +0900

    config file

--- config/index.js
+++
@@ -1,6 +0,0 @@
-const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
-const config = require(`./config.${env}.json`)
-
-module.exports = Object.freeze({
-  ...config,
-})
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,19 @@
 // eslint-disable-next-line no-undef
+const path = require('path')
+
 module.exports = {
   transpileDependencies: ['vuetify'],
   pages: {
     index: {
       entry: 'src/main.js',
       title: 'O-ROBOT',
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        config: path.resolve(`src/configs/config.${process.env.NODE_ENV}.js`),
+      },
     },
   },
   chainWebpack: (config) => {
