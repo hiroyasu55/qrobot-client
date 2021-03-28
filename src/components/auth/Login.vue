5c6a96e79f421a69f8d1ff75c50f98c7034fea8b
Hiroyasu Yamada <hiroyasu.yamada@logicheart.biz>
2021-03-28 22:00:16 +0900

    min-width

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -17,7 +17,8 @@ $box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%)
 
 .authenticator__wrapper {
   div[class^='Form__formSection'] {
-    width: 360px;
+    // width: 360px;
+    min-width: 360px;
     color: rgba(0, 0, 0, 0.87);
     border-radius: $border-radius;
     box-shadow: $box-shadow;
